b4b0e8030a819868770944b060b2cc406c7684a9
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-05-27 04:08:03 +0600

    BACKEND-ADD_1: apply environment port

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,7 +1,8 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
 
 const startServer=():void=>{
-    const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+    const port = config.port ? parseInt(config.port, 10) : 3000;
 
     if (isNaN(port) || port < 0 || port >= 65536) {
         throw new RangeError(`Invalid port number: ${port}. Port should be >= 0 and < 65536.`);
