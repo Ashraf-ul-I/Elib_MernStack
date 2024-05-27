55da3f85299ae7b26b9f9d3fffa25fd74f1f5360
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-05-28 02:37:33 +0600

    BACKEND_Add-7: signed jwt token

--- Backend/src/user/userController.ts
+++ Backend/src/user/userController.ts
@@ -2,6 +2,8 @@ import { NextFunction,Request,Response } from "express"
 import createHttpError from "http-errors"
 import User from './userModels'
 import bcrypt from 'bcrypt'
+import { sign } from "jsonwebtoken"
+import { config } from "../config/config"
 
 const createUser=async (
     req:Request,
@@ -21,17 +23,28 @@ const createUser=async (
     //database Call
 
     const user = await User.findOne({
-        email:email,
+        email:email, //means if the email is similiar to email then find
     })
+     //Process
     if(user){
         const error=createHttpError(404,'User already exist with this email')
         return next(error);
     }
     ///password not stored as plain
     const hashedPassword=await bcrypt.hash(password,10)
+   
+    const newUser=await User.create({
+       name,
+       email,
+       password:hashedPassword //in schema we name this as password so if we save like only 
+       //hashpassword it will throw error so we save that as password:hashedPassword
+    });
+
+    //Token Generation
+    const token= sign({sub:newUser._id},config.jwtSecret as string,{expiresIn:'7d'});
     
-    //Process
     //Response
+    res.json({accessToken:token});
     res.json({ message: "User registered" })
 }
 
