ac69fd9349a4a642c772adfcb635daca540f682d
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-05-28 01:56:00 +0600

    BACKEND_ADD-6:Add user model

--- Backend/src/user/userController.ts
+++ Backend/src/user/userController.ts
@@ -1,6 +1,6 @@
 import { NextFunction,Request,Response } from "express"
 import createHttpError from "http-errors"
-
+import User from './userModels'
 const createUser=async (
     req:Request,
 
@@ -16,6 +16,16 @@ const createUser=async (
         const error=createHttpError(400,"All Fields are required");
         return next(error);
     }
+    //database Call
+
+    const user = await User.findOne({
+        email:email,
+    })
+    if(user){
+        const error=createHttpError(404,'User already exist with this email')
+        return next(error);
+    }
+    
     //Process
     //Response
     res.json({ message: "User registered" })
